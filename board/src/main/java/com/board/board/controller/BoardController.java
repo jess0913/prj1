3bfd44748ebe445b5da3f9a78c580be8c122a981
jideangs <jongchans@hotmail.com>
2020-03-21 23:56:57 +0900

    메인화면

--- board/src/main/java/com/board/board/controller/BoardController.java
+++ board/src/main/java/com/board/board/controller/BoardController.java
@@ -2,9 +2,18 @@ package com.board.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
-
+	
+	@RequestMapping("/main")
+	public ModelAndView boardMain() {
+		ModelAndView mav = new ModelAndView();
+		
+		
+		mav.setViewName("jsp/board/main");
+		return mav;
+	}
 }
